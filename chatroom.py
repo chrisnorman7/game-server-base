62ea9ada8ddcbdd9dc74a1b27e7d99ebc09c29ba
Chris Norman <chris.norman2@googlemail.com>
2017-05-04 17:14:29 +0100

    Added help command to the chatroom.

--- chatroom.py
+++ chatroom.py
@@ -1,7 +1,24 @@
 """Test a running server."""
 
 import logging
-from gsb import Server
+from attr import attrs, attrib, Factory
+from gsb import Server, Command
+
+
+@attrs
+class MyCommand(Command):
+    """Give commands a help message."""
+    help = attrib(default=Factory(lambda: None))
+
+    def __attrs_post_init__(self):
+        super(MyCommand, self).__attrs_post_init__()
+        if self.help is None:
+            self.help = self.regexp.pattern
+            while self.help[0] in '^(':
+                self.help = self.help[1:]
+            while self.help[-1] in '$)':
+                self.help = self.help[:-1]
+            self.help = self.help.replace('|', ' or ')
 
 
 class MyServer(Server):
@@ -9,22 +26,57 @@ class MyServer(Server):
 
     def on_connect(self, caller):
         """Set a default nickname."""
+        self.notify(
+            caller.connection,
+            'Welcome to the chatroom. Type help for help.'
+        )
         caller.connection.nickname = caller.connection.host
+        self.broadcast('%s has connected.', caller.connection.nickname)
+
+    def on_disconnect(self, caller):
+        """Called when clients disconnect."""
+        self.broadcast('%s has disconnected.', caller.connection.nickname)
 
 
 logging.basicConfig(level='INFO')
 
-s = MyServer()
+s = MyServer(command_class=MyCommand)
 
 
-@s.command('^quit$')
+@s.command('^/quit$')
 def do_quit(caller):
     """Disconnect from the server."""
-    s.broadcast('%s disconnected.', caller.connection.nickname)
+    s.notify(caller.connection, 'Goodbye.')
     s.disconnect(caller.connection)
 
 
-@s.command('^(nick|nickname|name|handle) (?P<nickname>[^$]+)$')
+@s.command(
+    '^(/commands|\\?)$',
+    help='/commands or ?'
+)
+def do_commands(caller):
+    """Show all commands with brief help messages."""
+    s.notify(
+        caller.connection,
+        'Showing help for %d commands.',
+        len(s.commands)
+    )
+    for cmd in s.commands:
+        s.notify(
+            caller.connection,
+            '%s\n%s\n',
+            cmd.help,
+            cmd.func.__doc__ or 'No help available.'
+        )
+
+
+commands = ['nick', 'nickname', 'name', 'handle']
+
+
+@s.command(
+    '^\/(%s) (?P<nickname>[^$]+)$' % '|'.join(commands),
+    help='/%s <nickname>' % ' or '.join(commands)
+)
 def nickname(caller):
     """Set a nickname."""
     name = caller.match.groupdict()['nickname']
@@ -37,7 +89,10 @@ def nickname(caller):
         caller.connection.nickname = name
 
 
-@s.command('^[\:,`](?P<string>[^$]+)$')
+@s.command(
+    '^[\:,`](?P<string>[^$]+)$',
+    help='emote <anything> or :<anything>'
+)
 def emote(caller):
     """Emote something."""
     s.broadcast(
@@ -47,7 +102,10 @@ def emote(caller):
     )
 
 
-@s.command('^[^$]+$')
+@s.command(
+    '^[^$]+$',
+    help='Type anything else'
+)
 def speak(caller):
     """Say something."""
     s.broadcast('%s: %s', caller.connection.nickname, caller.text)
--- gsb/__init__.py
+++ gsb/__init__.py
@@ -7,10 +7,12 @@ from .server import Server
 from .protocol import Protocol
 from .factory import Factory
 from .caller import Caller
+from .command import Command
 
 __all__ = [
     'Server',
     'Protocol',
     'Factory',
+    'Command',
     'Caller'
 ]
