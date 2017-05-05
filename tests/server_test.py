5e00b63d7d56c415d3a6f537f8388b1b06bd9cbe
Chris Norman <chris.norman2@googlemail.com>
2017-05-05 09:13:26 +0100

    Added a command test.

--- tests/server_test.py
+++ tests/server_test.py
@@ -1,10 +1,26 @@
 """Test the Server class."""
 
-from gsb import Server
+from gsb import Server, Command, Caller
 
 s = Server()
+
+
+def command(caller):
+    """A test command."""
+    assert isinstance(caller, Caller)
 
 
 def test_init():
     assert s.connections == []
     assert s.banned_hosts == []
+
+
+def test_command():
+    s.commands.clear()
+    cmd = s.command('^$')(command)
+    assert isinstance(
+        cmd,
+        Command
+    )
+    assert cmd.func is command
+    assert s.commands == [cmd]
