b57734bbb818a4785894e9c390fe270b8faee6f0
Chris Norman <chris.norman2@googlemail.com>
2017-05-20 11:50:53 +0100

    Added a file of basic permissions.

---
+++ permissions.py
@@ -0,0 +1,29 @@
+"""Various allow functions for use with commands."""
+
+from attr import attrs, attrib, validators
+
+
+def anyone(caller):
+    """Always allow."""
+    return True
+
+
+@attrs
+class FuncPermission:
+    """return self.func(x(caller) for x in self.validators"""
+
+    validators = attrib(validator=validators.instance_of(tuple))
+    func = attrib()
+
+    def __call__(self, caller):
+        return self.func(x(caller) for x in self.validators)
+
+
+def and_(*validators):
+    """Ensure all validators pass."""
+    return FuncPermission(validators, any)
+
+
+def or_(*validators):
+    """Ensure any of the validators pass."""
+    return FuncPermission(validators, any)
